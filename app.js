51669f28ac726bcdf8c257e40de8e24a82d9d8cc
Enes Demirtaş <enesdemirtas255@gmail.com>
2023-10-06 18:34:35 +0200

    feat: add router (endpoints and actions) for comment model

--- app.js
+++ app.js
@@ -20,4 +20,7 @@ app.use("/authors", authorsRouter);
 const usersRouter = require("./routes/users");
 app.use("/users", usersRouter);
 
+const commentsRouter = require("./routes/comments");
+app.use("/comments", commentsRouter);
+
 app.listen(3000, () => console.log("Server started on port 3000"));
---
+++ routes/comments.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const router = express.Router();
+const Comment = require("../models/comment");
+
+// Get all comments
+router.get("/", async (req, res) => {
+  try {
+    const comments = await Comment.find().exec();
+    res.json(comments);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get comment by id
+router.get("/:id", getCommentById, (req, res) => {
+  res.send(res.comment);
+});
+
+// Create a comment
+router.post("/", async (req, res) => {
+  const comment = new Comment({
+    blog: req.body.blog,
+    comment: req.body.comment,
+    user: req.body.user,
+  });
+
+  try {
+    const newComment = await comment.save();
+    res.status(201).json(newComment);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Update a comment
+router.patch("/:id", getCommentById, async (req, res) => {
+  if (req.body.content != null) {
+    res.comment.content = req.body.content;
+  }
+
+  try {
+    const updatedComment = await res.comment.save();
+    res.json(updatedComment);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a comment
+router.delete("/:id", getCommentById, async (req, res) => {
+  try {
+    await res.comment.deleteOne();
+    res.json({ message: "The comment has been deleted." });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Private method - Get comment by id
+async function getCommentById(req, res, next) {
+  let comment;
+  try {
+    comment = await Comment.findById(req.params.id).exec();
+    if (comment == null) {
+      return res.status(404).json({ message: "Cannot find comment" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.comment = comment;
+  next();
+}
+
+module.exports = router;
